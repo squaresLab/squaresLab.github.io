2b3fae91cb25b88e57c16d51f09a004b9f6357a9
Travis CI <travis@travis-ci.org>
2018-08-01 18:27:33 -0400

    Something with git worktrees

--- push.sh
+++ push.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
+build_site() {
+    bundle exec jekyll build
+}
+
 setup_git() {
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
 }
 
 commit_site_files() {
-    git checkout -b test
+    git worktree add -b "build-$TRAVIS_BUILD_NUMBER" _site/ origin/test
+    bundle exec jekyll build
+    cd _site
     git add .
     git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
     git remote add origin-pages https://${GH_TOKEN}@github.com/squaresLab/squaresLab.github.io.git > /dev/null 2>&1
-    git push --quiet --set-upstream origin-pages test
+    git push --quiet --set-upstream origin-pages "build-$TRAVIS_BUILD_NUMBER"
 }
 
 setup_git
